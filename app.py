fec49ed1bd4e486d1c8fe6bd71b8cc06aa284782
jweel <jweelkar2016@gmail.com>
2024-07-15 00:08:36 +0600

    Adds functionality for add_book, view_books, search_books, remove_book, lend_books, return_books and a menu for make it user friendly

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ books_in_library = [
     {
         'title': 'Book_one',
         'authors': ['Author_one'],
-        'isbn': 12345,
+        'isbn': '12345-543',
         'publishing_year': 2000,
         'price': 200.00,
         'quantity': 20
@@ -11,7 +11,7 @@ books_in_library = [
     {
         'title': 'Book_two',
         'authors': ['Author_two'],
-        'isbn': 56789,
+        'isbn': '56789-768',
         'publishing_year': 2010,
         'price': 208.00,
         'quantity': 25
@@ -20,18 +20,26 @@ books_in_library = [
     {
         'title': 'Book_three',
         'authors': ['Author_three'],
-        'isbn': 127658,
+        'isbn': '127658-897',
         'publishing_year': 2014,
         'price': 215.00,
         'quantity': 15
     }
 ]
 
+books_lend = [
+
+    {
+        'title': 'Book_one',
+        'name': 'Alice',
+        'quantity': 2
+    }
+]
 
 def add_book():
     title = input('Enter book title: ')
     authors = input('Enter authors by space: ').split(' ')
-    isbn = int(input('Enter ISBN of book: '))
+    isbn = input('Enter ISBN of book: ')
     publishing_year = int(input('Enter book publishing year: '))
     price = float(input('Enter book price: '))
     quantity = int(input('Enter quantity: '))
@@ -62,12 +70,12 @@ def view_all_books():
             sep=' | ')
 
 
-def search_by_title():
-    search_term = input('Enter book title: ')
+def search_by_title_isbn():
+    search_term = input('Enter book title or ISBN: ')
 
     for book in books_in_library:
-        if search_term.lower() in book['title'].lower():
-            print(f"Found: {book['title']} - {book['authors']}")
+        if search_term.lower() in book['title'].lower() or search_term in book['isbn']:
+            print(f"Found: {book['title']} - {book['authors']} - {book['isbn']}")
 
 
 def search_by_authors():
@@ -91,16 +99,74 @@ def remove_contact():
     print('Contact remove successfully!')
 
 
+def lend_books():
+
+    book_title = input('Enter book title to lend(ex. Book_one): ')
+    borrower_name =input('Enter borrower name(ex. Alice): ')
+    quantity = int(input('Enter quantity of book(eg. 1): '))
+
+    for book in books_in_library:
+        if book_title == book['title']:
+            if book['quantity'] == 0:
+                print('Not enough books available to lend.')
+
+            else:
+                book['quantity'] -= quantity
+                print(f'Books remains: {book["quantity"]}')
+
+    lended_book = {
+        'title': book_title,
+        'name': borrower_name,
+        'quantity': quantity
+    }
+
+    books_lend.append(lended_book)
+
+    print(books_lend)
+
+
+def view_lended_book():
+    for book in books_lend:
+        print(
+            book['title'],
+            book['name'],
+            book['quantity'],
+            sep=' | '
+        )
+
+
+def book_return():
+
+    book_title = input('Enter book title to return(ex. Book_one): ')
+    borrower_name =input('Enter borrower name(ex. Alice): ')
+    quantity = int(input('Enter quantity of book(eg. 1): '))
+
+    returned_book = {
+        'title': book_title,
+        'name': borrower_name,
+        'quantity': quantity
+    }
+    
+    print(returned_book)
+
+    for book in books_in_library:
+        if book_title == book['title']:
+            book['quantity'] += quantity
+            print(f'Books remains: {book["title"]} | {book["quantity"]}')
+
 print('Welcome!')
 
 menu_text = """
 Your options:
 1. Add book
 2. View all books
-3. Search by title
+3. Search by title or ISBN
 4. Search by authors
 5. Remove contact
-6. Exit
+6. Lend books
+7. View lended books
+8. Book return
+0. Exit
 """
 
 while True:
@@ -114,7 +180,7 @@ while True:
         view_all_books()
 
     elif choice == '3':
-        search_by_title()
+        search_by_title_isbn()
 
     elif choice == '4':
         search_by_authors()
@@ -123,6 +189,15 @@ while True:
         remove_contact()
 
     elif choice == '6':
+        lend_books()
+
+    elif choice == '7':
+        view_lended_book()
+
+    elif choice == '8':
+        book_return()
+
+    elif choice == '0':
         break
     
     else:
